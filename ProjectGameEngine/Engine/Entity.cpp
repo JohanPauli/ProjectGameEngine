875d1cbf69132471e32fa1a80d8007996b0187e5
Karif <karifrederiksen@gmail.com>
2015-03-12 18:37:37 +0000

    added nullptr protection for accessors

--- ProjectGameEngine/Engine/Entity.cpp
+++ ProjectGameEngine/Engine/Entity.cpp
@@ -40,14 +40,14 @@ void Entity::render(Renderer* renderer) {
 	graphics->render(*this, renderer);
 }
 
-Rect Entity::getRect() const	{ return std::move(physics->getRect()); }
-double Entity::getAngle() const { return physics->getAngle(); }
-int Entity::getX() const		{ return (int)physics->getXPosition(); }
-int Entity::getY() const		{ return (int)physics->getYPosition(); }
-int Entity::getWidth() const	{ return (int)physics->getWidth(); }
-int Entity::getHeight() const	{ return (int)physics->getHeight(); }
-float Entity::getXvelocity() const { return physics->getXVelocity(); }
-float Entity::getYvelocity() const { return physics->getYVelocity(); }
-void Entity::setXvelocity(float vel) const { physics->setXVelocity(vel); }
-void Entity::setYvelocity(float vel) const { physics->setYVelocity(vel); }
+Rect Entity::getRect() const	{ return physics == nullptr ? Rect() :	std::move(physics->getRect()); }
+double Entity::getAngle() const { return physics == nullptr ? 0 :	physics->getAngle(); }
+int Entity::getX() const		{ return physics == nullptr ? 0 :(int)physics->getXPosition(); }
+int Entity::getY() const		{ return physics == nullptr ? 0 :(int)physics->getYPosition(); }
+int Entity::getWidth() const	{ return physics == nullptr ? 0 :(int)physics->getWidth(); }
+int Entity::getHeight() const	{ return physics == nullptr ? 0 :(int)physics->getHeight(); }
+float Entity::getXvelocity() const { return physics == nullptr ? 0 : physics->getXVelocity(); }
+float Entity::getYvelocity() const { return physics == nullptr ? 0 : physics->getYVelocity(); }
+void Entity::setXvelocity(float vel) const { if (physics != nullptr) physics->setXVelocity(vel); }
+void Entity::setYvelocity(float vel) const { if (physics != nullptr) physics->setYVelocity(vel); }
 InputContext* Entity::getInputContext() const { return input; }
